2905727a0d36fcdff9d61234d6b7e2668fd002bd
Swaraj Gandhi <swaraj@browserstack.com>
2020-03-01 19:45:02 +0530

    added gitignore

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-// import React from 'react';
-// import logo from './logo.svg';
 import React, { Component } from 'react'
 import {debounce} from 'lodash';
 import './App.css';
@@ -12,7 +10,6 @@ class App extends Component {
     this.state = {
       searchTerm: props.searchTerm
     };
-    // this.onChange = debounce(this.onChange.bind(this), 100);
   }
 
   onChange = debounce((searchTerm) => {
@@ -29,28 +26,6 @@ class App extends Component {
       </div>
     );
   }
-
 }
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React Dhruva
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 export default App;
